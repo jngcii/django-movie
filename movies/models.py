2ac4e81b87c8f0f35a9f17be5c55b66231121e93
jngcii <concotree@gmail.com>
2020-05-20 17:01:40 +0900

    Replace Tag models front

--- movies/models.py
+++ movies/models.py
@@ -4,6 +4,13 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+class Tag(models.Model):
+    """
+    genre에 해당하는 모델
+    """
+    name = models.CharField(max_length=100)
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     original_title = models.CharField(max_length=255)
@@ -23,13 +30,6 @@ class Movie(models.Model):
     @property
     def bad_seho(self):
         return self.sehos.filter(is_like=False).count()
-
-
-class Tag(models.Model):
-    """
-    genre에 해당하는 모델
-    """
-    name = models.CharField(max_length=100)
 
 
 class Seho(models.Model):
