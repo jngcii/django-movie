cb78acc6ef7b5cdaeaa3f610b1827b5c9204ec80
jngcii <concotree@gmail.com>
2020-05-20 17:33:56 +0900

    Replace seho views remark

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.db.models imoprt Q
+from django.db.models import Q
 from .models import Movie, Tag, Seho
 
 
@@ -26,37 +26,37 @@ def detail(request, movie_id):
     return render(request, 'movies/detail.html', context)
 
 
-def good_seho(request, movie_id):
-    movie = get_object_or_404(Movie, id=movie_id)
-    user = request.user
-    try:
-        seho = Seho.objects.get(creator=user, movie=movie)
-        if seho.is_like:
-            # status = 400
-        else:
-            seho.is_like = True
-            # status = 200
-    except Seho.DoesNotExist:
-        seho = Seho.objects.create(is_like=True, creator=user, movie=movie)
-        if seho:
-            # status = 200
-        else:
-            # status = 400
+# def good_seho(request, movie_id):
+#     movie = get_object_or_404(Movie, id=movie_id)
+#     user = request.user
+#     try:
+#         seho = Seho.objects.get(creator=user, movie=movie)
+#         if seho.is_like:
+#             # status = 400
+#         else:
+#             seho.is_like = True
+#             # status = 200
+#     except Seho.DoesNotExist:
+#         seho = Seho.objects.create(is_like=True, creator=user, movie=movie)
+#         if seho:
+#             # status = 200
+#         else:
+#             # status = 400
 
 
-def bad_seho(request, movie_id):
-    movie = get_object_or_404(Movie, id=movie_id)
-    user = request.user
-    try:
-        seho = Seho.objects.get(creator=user, movie=movie)
-        if seho.is_like:
-            seho.is_like = False
-            # status = 200
-        else:
-            # status = 400
-    except Seho.DoesNotExist:
-        seho = Seho.objects.create(is_like=False, creator=user, movie=movie)
-        if seho:
-            # status = 200
-        else:
-            # status = 400
+# def bad_seho(request, movie_id):
+#     movie = get_object_or_404(Movie, id=movie_id)
+#     user = request.user
+#     try:
+#         seho = Seho.objects.get(creator=user, movie=movie)
+#         if seho.is_like:
+#             seho.is_like = False
+#             # status = 200
+#         else:
+#             # status = 400
+#     except Seho.DoesNotExist:
+#         seho = Seho.objects.create(is_like=False, creator=user, movie=movie)
+#         if seho:
+#             # status = 200
+#         else:
+#             # status = 400
