4d1b3e17f8faba2acd9f1416bce5c22d0e9f3e7d
jngcii <concotree@gmail.com>
2020-05-20 16:25:07 +0900

    Add Movie, Tag, Seho, Review model in movies model

--- movies/models.py
+++ movies/models.py
@@ -1,9 +1,39 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
 
 class Movie(models.Model):
-    pass
+    title = models.CharField(max_length=255)
+    original_title = models.CharField(max_length=255)
+    release_date = models.DateField()
+    adult = models.BooleanField()
+    overview = models.TextField()
+    poster = models.URLField()
+    tags = models.ManyToManyField(Tag, blank=True, related_name='movies')
+
+    def __str__(self):
+        return '{} : {}'.format(self.title, self.release_date)
+
+
+class Tag(models.Model):
+    """
+    genre에 해당하는 모델
+    """
+    name = models.CharField(max_length=100)
+
+
+class Seho(models.Model):
+    """
+    like or unlike movie
+    """
+    is_like = models.BooleanField()
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='sehos')
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sehos')
+
 
 class Review(models.Model):
-    pass
+    movie = models.ForeignKey(Movie)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    caption = models.TextField()
